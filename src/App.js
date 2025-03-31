db87b6933a791362161d3352f9ac1a74ac600f44
Prakruti panchal <prakrutipanchal2005@gmail.com>
2025-03-31 14:34:24 +0530

    URl changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 function App() {
   useEffect(() => {
-    const socket = io('http://localhost:3001'); // Replace with your backend URL after deployment
+    const socket = io('https://socketio-backend.onrender.com'); // Replace with your Render/Railway URL// Replace with your backend URL after deployment
 
     socket.on('welcome', (msg) => {
       console.log('Server says:', msg); // Check browser console
